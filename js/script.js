f5998d839a7d27956421d9f37b8ef8aafb8fcd85
NazarineWasonga <nazarinewasonga48@gmail.com>
2020-12-14 23:20:10 +0300

    Edited script.Js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var females = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var months = ["January", "Feburary", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+
 
 
 function calc() {
-    event.preventDefault();
-    var day = document.getElementById('day');
-    var year = document.getElementById('year');
-    var month = document.getElementById('month');
+    var days = document.getElementById('day').value;
+    var year = document.getElementById('year').value;
+    var month = document.getElementById('month').value;
     var gender = document.getElementById('gender').value;
-    dates = new Date(year);
-    var days = dates.getDay();
+
+    var today = new Date();
+    // var dd   = today.getDate(days);
+    var mm = months[today.getMonth(month)];
+    var yyyy = today.getFullYear(year);
+    var anyDay = days[today.getDay()];
 
     // Invalid Day
     if ((day <= 0) || (day > 31)) {
@@ -26,12 +33,13 @@ function calc() {
     if (year < 1900) {
         alert("Are you that old!");
     }
-    if (gender === 'male') {
-        alert(days[dates.getDay()] + ',' + " your name is: " + males[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[dates.getDay()] + " . ");
-    } else if (gender === 'female') {
-        alert(days[dates.getDay()] + ',' + " your name is: " + females[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[dates.getDay()] + " . ");
 
+
+    if (gender === 'male') {
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[anyDay] + ".");
+
+    } else if (gender === 'female') {
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " +females[anyDay] + ".");
+    
     } else { }
 }
