2c37fddd0da3ced1dea65d281c29ecd53b067363
NazarineWasonga <nazarinewasonga48@gmail.com>
2020-12-14 12:53:28 +0300

    Edited script.js

--- js/script.js
+++ js/script.js
@@ -2,44 +2,48 @@ var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var females = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
+
+function calc() {
 event.preventDefault();
 var day = document.getElementById('day');
 var year = document.getElementById('year');
 var month = document.getElementById('month');
 var gender = document.getElementById('gender').value;
-dates = new Date(year);
-var days = dates.getDay();
+dates = new Date(eval(year).value);
+//var days = dates.getDay();
+
+function getTheDay() {
+    date = new Date(eval(year).value);
+    //return days[myDate.getDay()]
+}
 
 // Invalid Day
 if ((day <= 0) || (day > 31)) {
-    alert("Day is out of range!");
+    alert("please enter valid day");
 }
 
 //Invalid Month 
 if ((month <= 0) || (month > 12)) {
-    alert("Month is out of range!");
+    alert("please enter valid month");
 }
 
 // Invalid Year
 if (year < 1900) {
-    alert("You're not really that old!");
+    alert("Are you that old!");
 }
 
 if (gender === 'male') {
-    return maleArray(7);
-} else {
-    return femaleArray(7);
-};
-
-if (gender === 'male') {
-    alert(days[dates.getDay()] + ',' + " your name is: " + males[dates.getDay()]);
-    document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[dates.getDay()] + " . ");
-    if (gender === 'male') {
-        alert(days[dates.getDay()] + ',' + " your name is: " + males[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[dates.getDay()] + " . ");
-    } else if (gender === 'female') {
-        alert(days[dates.getDay()] + ',' + " your name is: " + females[dates.getDay()]);
-        document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[dates.getDay()] + " . ");
-
-    } else { }
+   alert("Your name is:"+ males[date.getDay()]);
+   document.getElementById('your-name').innerHTML=("Your Name Is"+males[date.getDay()]+".");
 }
+else if  (gender === 'female') {
+    alert("Your name is"+ females[date.getDay()]);
+    document.getElementById('your-name').innerHTML=("Your Name Is:"+females[date.getDay()]+".");
+
+}else{}
+
+}
+
+   
+
+
