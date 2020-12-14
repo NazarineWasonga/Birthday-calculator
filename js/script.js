bfe0948a43af76f3ad64fa9fbc63173f757d3df5
NazarineWasonga <nazarinewasonga48@gmail.com>
2020-12-14 18:05:38 +0300

    updated script .js

--- js/script.js
+++ js/script.js
@@ -4,46 +4,34 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 
 function calc() {
-event.preventDefault();
-var day = document.getElementById('day');
-var year = document.getElementById('year');
-var month = document.getElementById('month');
-var gender = document.getElementById('gender').value;
-dates = new Date(eval(year).value);
-//var days = dates.getDay();
+    event.preventDefault();
+    var day = document.getElementById('day');
+    var year = document.getElementById('year');
+    var month = document.getElementById('month');
+    var gender = document.getElementById('gender').value;
+    dates = new Date(year);
+    var days = dates.getDay();
 
-function getTheDay() {
-    date = new Date(eval(year).value);
-    //return days[myDate.getDay()]
+    // Invalid Day
+    if ((day <= 0) || (day > 31)) {
+        alert("please enter valid day");
+    }
+
+    //Invalid Month 
+    if ((month <= 0) || (month > 12)) {
+        alert("please enter valid month");
+    }
+
+    // Invalid Year
+    if (year < 1900) {
+        alert("Are you that old!");
+    }
+    if (gender === 'male') {
+        alert(days[dates.getDay()] + ',' + " your name is: " + males[dates.getDay()]);
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " + males[dates.getDay()] + " . ");
+    } else if (gender === 'female') {
+        alert(days[dates.getDay()] + ',' + " your name is: " + females[dates.getDay()]);
+        document.getElementById('your-name').innerHTML = ("Your Name Is: " + females[dates.getDay()] + " . ");
+
+    } else { }
 }
-
-// Invalid Day
-if ((day <= 0) || (day > 31)) {
-    alert("please enter valid day");
-}
-
-//Invalid Month 
-if ((month <= 0) || (month > 12)) {
-    alert("please enter valid month");
-}
-
-// Invalid Year
-if (year < 1900) {
-    alert("Are you that old!");
-}
-
-if (gender === 'male') {
-   alert("Your name is:"+ males[date.getDay()]);
-   document.getElementById('your-name').innerHTML=("Your Name Is"+males[date.getDay()]+".");
-}
-else if  (gender === 'female') {
-    alert("Your name is"+ females[date.getDay()]);
-    document.getElementById('your-name').innerHTML=("Your Name Is:"+females[date.getDay()]+".");
-
-}else{}
-
-}
-
-   
-
-
