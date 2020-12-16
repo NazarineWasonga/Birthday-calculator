d91d99502cc6544944d7ea2a6abcf99918f614e4
NazarineWasonga <nazarinewasonga48@gmail.com>
2020-12-16 17:56:46 +0300

    Edited spelling on names

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var males = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var females = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var females = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 
